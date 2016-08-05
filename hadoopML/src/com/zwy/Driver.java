f9da8cbcb58eb52e2d2cac09c8b380a9083c9888
iam91 <zhengwy91@foxmail.com>
2016-08-06 00:21:22 +0800

    update

--- hadoopML/src/com/zwy/Driver.java
+++ hadoopML/src/com/zwy/Driver.java
@@ -1,18 +1,64 @@
 package com.zwy;
 
-import com.zwy.core.FileReader;
-import com.zwy.core.DataSet;
 
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.ArrayPrimitiveWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+import com.zwy.ml.logisticregression.ArrayStringTrans;
+import com.zwy.ml.logisticregression.LogisticRegressionMapper;
+import com.zwy.ml.logisticregression.LogisticRegressionReducer;
 
 import java.io.IOException;
 
 public class Driver {
-	public static void main(String[] args) throws IOException{
-		String file = "/home/zwy/workspace/miscellaneous/data_banknote_authentication.csv";
+	public static void main(String[] args) throws IOException, ClassNotFoundException, InterruptedException{
+		String file = "/home/zwy/workspace/miscellaneous/resources/data_banknote_authentication.csv";
+		
+		double alpha = 0.05;
+		int numOfIteration = 100;
+		int numOfFeature = 5;
+		int numOfInstances = 1372;
+		int targetValIndex = numOfFeature - 1;
+		double[] theta = new double[numOfFeature];
+		for(int i = 0; i < theta.length; i++){
+			theta[i] = 0.0;
+		}
+		
 		Configuration conf = new Configuration();
-		FileReader reader = new FileReader(file, conf);
-		DataSet dataSet = reader.getDataSet();
-		System.out.println(dataSet.getData().length);
+		
+		conf.setDouble("ALPHA", alpha);
+		conf.setInt("N_ITER", numOfIteration);
+		conf.setInt("N_FEATURE", numOfFeature);
+		conf.setInt("N_INSTANCE", numOfInstances);
+		conf.setInt("TARGET_ID", targetValIndex);
+		conf.set("THETA", ArrayStringTrans.doubleArrayToString(theta));
+		
+		System.out.println(conf.get("THETA"));
+		
+		//for()
+		
+		Job job = Job.getInstance(conf, "Test");
+		job.setJarByClass(com.zwy.Driver.class);
+		
+		job.setMapperClass(LogisticRegressionMapper.class);
+		job.setReducerClass(LogisticRegressionReducer.class);
+		
+		job.setMapOutputKeyClass(LongWritable.class);
+		job.setMapOutputValueClass(ArrayPrimitiveWritable.class);
+		
+		job.setOutputKeyClass(Text.class);
+		job.setOutputValueClass(ArrayPrimitiveWritable.class);
+		
+		FileInputFormat.setInputPaths(job, new Path(file));
+		FileOutputFormat.setOutputPath(job, new Path(file + "-out"));
+		
+		if (!job.waitForCompletion(true)){
+		}
 	}
 }
---
+++ hadoopML/src/com/zwy/ml/logisticregression/ArrayStringTrans.java
@@ -0,0 +1,20 @@
+package com.zwy.ml.logisticregression;
+
+public class ArrayStringTrans {
+	public static String doubleArrayToString(double[] array){
+		String ret = "";
+		for(int i = 0; i < array.length; i++){
+			ret += (String.valueOf(array[i]) + (i < array.length - 1 ? "," : ""));
+		}
+		return ret;
+	}
+	
+	public static double[] stringToDoubleArray(String s){
+		String[] split = s.split(",");
+		double[] ret = new double[split.length];
+		for(int i = 0; i < split.length; i++){
+			ret[i] = Double.parseDouble(split[i]);
+		}
+		return ret;
+	}
+}
